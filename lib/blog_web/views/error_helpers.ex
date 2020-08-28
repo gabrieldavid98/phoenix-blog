2c314932b0b247e9cef6b4451fb5dae8eb1440e1
Gabriel David <gabrieldavid_98@hotmail.com>
2020-08-27 23:57:27 -0500

    add some styles

--- lib/blog_web/views/error_helpers.ex
+++ lib/blog_web/views/error_helpers.ex
@@ -10,8 +10,8 @@ defmodule BlogWeb.ErrorHelpers do
   """
   def error_tag(form, field) do
     Enum.map(Keyword.get_values(form.errors, field), fn error ->
-      content_tag(:span, translate_error(error),
-        class: "invalid-feedback",
+      content_tag(:p, translate_error(error),
+        class: "text-red-500 text-xs italic",
         phx_feedback_for: input_id(form, field)
       )
     end)
